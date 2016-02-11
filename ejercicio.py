3beb1b493d5a5e7c454ba9d942e0dbe10b531cd7
Marina Martin Hernandez <mmartin@zeta40.aulas.gsyc.es>
2016-02-11 09:31:33 +0100

    Ejercicio 3

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# hola, hola
+# Marina Martín Hernández
+# Ejercicio 13.4:
 
-fd = open('/etc/passwd', 'r')
+fd = open('/etc/passwd','r')
+lines = fd.readlines()
+dicc = {}
 
-lineas = fd.readlines()
-fd.close()
+for line in lines:
+	element = line.split(':')
+	user = element[0]
+	shell = element[-1][:-1]
+	dicc[user] = shell
+print dicc['root']
+try:
+	print dicc['Imaginary']
+except : 
+	print ("\nIncorrect User")
 
-for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
